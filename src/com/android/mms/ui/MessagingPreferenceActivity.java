fcb36fdc27f98b37de3d65fae389574e8cc67201
Eagle1967 <icsourcery@gmail.com>
2012-05-12 16:05:30 -0600

    Added "Support for emojis" on the MMS app + Quick emojis - gferon

--- src/com/android/mms/ui/MessagingPreferenceActivity.java
+++ src/com/android/mms/ui/MessagingPreferenceActivity.java
@@ -63,6 +63,9 @@ public class MessagingPreferenceActivity extends PreferenceActivity
     public static final String RETRIEVAL_DURING_ROAMING = "pref_key_mms_retrieval_during_roaming";
     public static final String AUTO_DELETE              = "pref_key_auto_delete";
     public static final String DISPLAY_FULLDATE         = "pref_key_display_fulldate";
+    public static final String ENABLE_EMOJIS            = "pref_key_enable_emojis";
+    public static final String FULL_TIMESTAMP           = "pref_key_mms_full_timestamp";
+    public static final String ENABLE_QUICK_EMOJIS      = "pref_key_emojis_quick";
 
     // Menu entries
     private static final int MENU_RESTORE_DEFAULTS    = 1;
